00b22e1701151cebbc61fb135b0182c20911b69b
Eric <ericmontelares3@gmail.com>
2020-04-17 15:45:01 -0300

    fix plugins

--- lib/tabs/about_tabs/community_tab.dart
+++ lib/tabs/about_tabs/community_tab.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_youtube/flutter_youtube.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:youtube_player/youtube_player.dart';
 
 class CommunityPage extends StatelessWidget {
   @override
