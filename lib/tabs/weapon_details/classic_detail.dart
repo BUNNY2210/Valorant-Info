48d085fac183f48adffa34983766d3904c59e76b
Eric <ericmontelares3@gmail.com>
2020-04-20 09:35:46 -0300

    new weapon detail

--- lib/tabs/weapon_details/classic_detail.dart
+++ lib/tabs/weapon_details/classic_detail.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Classic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromRGBO(14, 76, 71, 1),
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                  icon: FaIcon(
+                    FontAwesomeIcons.chevronDown,
+                    size: 25,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  });
+            },
+          ),
+          title: Text("CLASSIC"),
+          backgroundColor: Color.fromRGBO(14, 24, 35, 1),
+        ),
+        backgroundColor: Color.fromRGBO(32, 43, 67, 1),
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
               Card(
-                color: Color.fromRGBO(48, 103, 98, 1),
+                color: Color.fromRGBO(24, 35, 56, 1),
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
+                    SizedBox(
+                      height: 2,
+                    ),
                     Image.network(
                         'https://trackercdn.com/cdn/tracker.gg/valorant/db/weapons/classic-standard.png',
                         height: 215,
@@ -41,50 +63,55 @@ class Classic extends StatelessWidget {
                             style: TextStyle(color: Colors.white),
                           ),
                         ),
+                        RaisedButton(
+                            color: Colors.transparent,
+                            child: Row(
+                              children: <Widget>[
+                                Text('Preview',
+                                    style: TextStyle(color: Colors.white)),
+                                SizedBox(
+                                  width: 4,
+                                ),
+                                FaIcon(
+                                  FontAwesomeIcons.youtube,
+                                  size: 25,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                            onPressed: () {
+                              Route route = MaterialPageRoute(
+                                  builder: (context) => ClassicFire());
+                              Navigator.push(context, route);
+                            }),
                       ],
                     ),
                   ],
                 ),
               ),
               Card(
-                color: Color.fromRGBO(48, 103, 98, 1),
+                color: Color.fromRGBO(32, 43, 67, 1),
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
-                    Image.network(
-                        'https://trackercdn.com/cdn/tracker.gg/valorant/db/weapons/shorty-standard.png',
-                        height: 215,
-                        width: 500),
-                    ButtonBar(
-                      children: <Widget>[
-                        Align(
-                          alignment: Alignment(0.2, 0.6),
-                          child: Text(
-                            'Shorty',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 3,
-                        ),
-                        FaIcon(
-                          FontAwesomeIcons.coins,
-                          color: Colors.yellow,
-                        ),
-                        Align(
-                          alignment: Alignment(0.2, 0.6),
-                          child: Text(
-                            '200',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
+                    Image.asset('img/classic.png', height: 400, width: 500),
                   ],
                 ),
               ),
             ],
           ),
         ));
+  }
+}
+
+class ClassicFire extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      withJavascript: true,
+      withLocalStorage: true,
+      url:
+          "https://blitz-cdn-videos.blitz.gg/valorant/weapons/classic/spray_footage/classic_primary2.mp4",
+    );
   }
 }
