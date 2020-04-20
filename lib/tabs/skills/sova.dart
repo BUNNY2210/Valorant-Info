7d0df885e8316ce65083a43e209d21c52b3db38f
Eric <ericmontelares3@gmail.com>
2020-04-20 15:43:20 -0300

    fix sova

--- lib/tabs/skills/sova.dart
+++ lib/tabs/skills/sova.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:valorantinfo/tabs/skills/breach.dart';
 
 class Sova extends StatelessWidget {
   @override
